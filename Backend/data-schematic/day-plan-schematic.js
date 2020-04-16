4fa1e3ff43895be113db818006946dd4437a3915
way2key <olivier_dancona@hotmail.com>
2020-04-16 11:06:16 +0200

    Implemented admin-data-student search
    J'ai implémenté la fonction de recherche des stagiaires.
    J'ai aussi complété le schéma du dayplan

--- Backend/data-schematic/day-plan-schematic.js
+++ Backend/data-schematic/day-plan-schematic.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
 const dayPlanSchema = mongoose.Schema({
+  startOfDay: {type: String, required:true, default:"06:00:00"},
+  endOfDay: {type: String, required:true, default:"22:00:00"},
   requiredTime: {type: Number, required:true},
   shift: {type: [String], required:true},
 });
--- src/app/admin/admin-data-student/admin-data-student.component.ts
+++ src/app/admin/admin-data-student/admin-data-student.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-admin-data-student',
@@ -8,15 +11,36 @@ import { Component, OnInit } from '@angular/core';
 export class AdminDataStudentComponent implements OnInit {
   students = [];
   shownStudents = [];
+  searchField = '';
+  interaction = new FormControl('');
+  options: string[] = ['One', 'Two', 'Three'];
+  filteredOptions: Observable<string[]>;
   constructor() { }
 
   ngOnInit(): void {
     this.getStudents();
+    this.filteredOptions = this.interaction.valueChanges
+    .pipe(
+      startWith(''),
+      map(value =>  this._filter(value))
+    );
   }
 
   getStudents(): void {
-    this.students.push({firstname:"Olivier",lastname:"Dancona"});
-    this.shownStudents = this.students;
+    this.students.push({firstname:"Olivier",lastname:"D'Ancona"},{firstname:"Ryan",lastname:"Quinn"});
+  }
+
+  clearSearchField() {
+    this.searchField = '';
+  }
+
+  private _filter(option: string): string[] {
+    const filterValue = option.toLowerCase();
+    this.shownStudents = this.students.filter(student => {
+      let search = (student.firstname + " " + student.lastname).toLowerCase();
+      return (search.includes(filterValue));
+    });
+    return this.shownStudents.map(student => (student.firstname + " " + student.lastname))
   }
 
 }
